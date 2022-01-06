e6f890e2f3d9830c628f7f171ffcef9e8e6edb67
Ratan <ratankapr22@gmail.com>
2022-01-06 10:50:51 +0530

    added one file A.java and modified Demo.java

---
+++ studentregistrationapp/src/main/java/com/cg/app/A.java
@@ -0,0 +1,8 @@
+package com.cg.app;
+
+public class A {
+
+	public void funA() {
+		System.out.println("inside funA of A");
+	}
+}
--- studentregistrationapp/src/main/java/com/cg/app/Demo.java
+++ studentregistrationapp/src/main/java/com/cg/app/Demo.java
@@ -5,6 +5,13 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is Demo main class");
+
+System.out.println("this is from ratan....");
+
+
+A a1=new A();
+
+a1.funA();
 	}
 
 }
