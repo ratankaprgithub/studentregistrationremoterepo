c654ddc9477583384b767420d58a43425eaeb23d
Ratan <ratankapr22@gmail.com>
2022-01-06 10:41:10 +0530

    first initial commit

---
+++ studentregistrationapp/src/main/java/com/cg/app/StudentregistrationappApplication.java
@@ -0,0 +1,13 @@
+package com.cg.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentregistrationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentregistrationappApplication.class, args);
+	}
+
+}
